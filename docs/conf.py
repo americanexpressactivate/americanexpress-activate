d7b38ccb9c16d2a63f93646c1c59ac918f2c64e0
americanexpressactivate <bullyalina@gmail.com>
2026-08-14 13:27:02 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,15 @@
+project = "AmericanExpress.com/Activate"
+author = "AmericanExpress.com/Activate"
+release = "1.0"
 
+extensions = []
+
+templates_path = []
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+
+html_title = "AmericanExpress.com/Activate – Amex Card Activation Guide"
